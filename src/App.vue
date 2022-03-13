d53ad37174040d094193adbf6665bac14fbb53c3
habarahonaa <habarahonaa@gmail.com>
2022-03-13 10:10:31 -0600

    Finalized design, still issues with file paths

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,62 @@
+<script>
+import DestinationCard from './components/DestinationCard.vue';
+
+export default {
+  name: 'app',
+  components: {
+    DestinationCard,
+  },
+  data() {
+    return {
+      popularDestinations: [
+        {
+          city: 'Toronto',
+          averagePrice: 120,
+          propertyCount: 76,
+          imageUrl: './img/tor.jpg',
+          imageAlt: 'Toronto skyline',
+        },
+        {
+          city: 'Malibu',
+          averagePrice: 215,
+          propertyCount: 43,
+          imageUrl: './img/mali.jpg',
+          imageAlt: 'Cliff in Malibu',
+        },
+        {
+          city: 'Chicago',
+          averagePrice: 130,
+          propertyCount: 115,
+          imageUrl: './img/chi.jpg',
+          imageAlt: 'Chicago skyline',
+        },
+        {
+          city: 'Seattle',
+          averagePrice: 135,
+          propertyCount: 63,
+          imageUrl: './img/seatt.jpg',
+          imageAlt: 'Seattle skyline',
+        },
+        {
+          city: 'Colorado',
+          averagePrice: 85,
+          propertyCount: 47,
+          imageUrl: './img/col.jpg',
+          imageAlt: 'Lake in Colorado',
+        },
+        {
+          city: 'Miami',
+          averagePrice: 115,
+          propertyCount: 86,
+          imageUrl: './img/miam.jpg',
+          imageAlt: 'Beach in Miami',
+        },
+      ],
+    };
+  },
+};
+</script>
+
 <template>
   <div class="bg-gray-100 flex">
     <div
@@ -13,7 +72,7 @@
       <div class="xl:max-w-lg xl:ml-auto">
         <img src="./img/logo.svg" alt="Workcation Logo" class="h-10" />
         <img
-          src="./img/beach-work.jpeg"
+          src="./img/workcationing.jpg"
           alt="Person relaxing at the beach"
           class="
             mt-6
@@ -59,10 +118,26 @@
     </div>
     <div class="hidden lg:block lg:w-1/2 lg:relative">
       <img
-        src="./img/beach-work.jpeg"
+        src="./img/workcationing.jpg"
         alt="Person relaxing at the beach"
         class="absolute inset-0 h-full w-full object-cover object-center"
       />
+    </div>
+  </div>
+  <div>
+    <div class="max-w-md sm:max-w-xl lg:max-w-6xl mx-auto px-8 lg:px-12 py-8">
+      <h2 class="text-xl text-gray-900">Popular destinations</h2>
+      <p class="text-gray-600">
+        A selection of great work-friendly cities with lots to see and explore.
+      </p>
+      <div class="flex flex-wrap -mx-4">
+        <div
+          class="mt-6 w-full px-4 lg:w-1/2 xl:w-1/3"
+          v-for="destination in popularDestinations"
+        >
+          <DestinationCard :destination="destination" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
